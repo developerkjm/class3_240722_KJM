6dad90a3482e0057d14241820670e12e3fc57023
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2024-11-05 15:50:30 +0900

    주석 commit 테스트

--- Git_Test_KJM/src/main/java/com/itwillbs/gittest_kjm/HomeController.java
+++ Git_Test_KJM/src/main/java/com/itwillbs/gittest_kjm/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		
+		// index 주석 저장
 		return "index";
 	}
 	
